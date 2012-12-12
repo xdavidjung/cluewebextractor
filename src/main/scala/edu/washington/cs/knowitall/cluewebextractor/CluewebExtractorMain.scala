efc833f7532b94df171cba2feaf24eb00490584b
David Jung (Work) <xdavidjung@gmail.com>
2012-12-11 17:11:37 -0800

    added logic for printing out relevant information post-processing

--- src/main/scala/edu/washington/cs/knowitall/cluewebextractor/CluewebExtractorMain.scala
+++ src/main/scala/edu/washington/cs/knowitall/cluewebextractor/CluewebExtractorMain.scala
@@ -1,20 +1,39 @@
 package edu.washington.cs.knowitall.cluewebextractor
 import scala.io.Source;
 
-import de.l3s.boilerpipe;
+import de.l3s.boilerpipe.extractors;
+import edu.washington.cs.knowitall.tool.sentence.OpenNlpSentencer;
 
 // takes a filename as a command-line argument and outputs a TODO
 object CluewebExtractorMain extends App {
+  val bp = extractors.ArticleSentencesExtractor.getInstance
+  val sentencer = new OpenNlpSentencer()
+
   // check for the proper number of command line arguments
   if (args.length != 1) usage();
 
   // get an iterator over the lines of the file specified
-  val warcIt = new WarcEntryIterator(Source.fromFile(args(0)).getLines);
+  val warcIt = new WarcEntryIterator(Source.fromFile(args(0), "iso-8859-1").getLines);
 
   while(warcIt.hasNext) {
-    // TODO write out the warc entry in an extractable format
     val warc = warcIt.next()
-    if (warc != null) println(warc.warcTrecId + " length: " + warc.contentLength)
+    if (warc != null) {
+      // pass through boilerpipe and sentencer
+      val piped = bp.getText(warc.payload)
+      val sentences = sentencer.sentences(piped)
+
+      // for each sentence, print out the:
+      // id, uri, sentence number, sentence
+      // separated by tabs
+      var i = 0;
+      for (sentence <- sentences) {
+        println(warc.warcTrecId + "\t" +
+                warc.warcTargetUri + "\t" +
+                i.toString + "\t" +
+                sentence)
+        i = i + 1;
+      }
+    }
   }
 
   def usage() {
