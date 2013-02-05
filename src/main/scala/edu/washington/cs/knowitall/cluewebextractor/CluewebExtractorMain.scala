5f69d1ce0483e4c462c9b4f37ec586fdf3f73d82
David Jung (Work) <xdavidjung@gmail.com>
2013-02-05 14:57:59 -0800

    Now takes as input a directory; changed logic to account for language detection.

--- src/main/scala/edu/washington/cs/knowitall/cluewebextractor/CluewebExtractorMain.scala
+++ src/main/scala/edu/washington/cs/knowitall/cluewebextractor/CluewebExtractorMain.scala
@@ -4,45 +4,56 @@ import scala.io.Source;
 import de.l3s.boilerpipe.extractors;
 import edu.washington.cs.knowitall.tool.sentence.OpenNlpSentencer;
 
-// takes a filename as a command-line argument and outputs a TODO
+// CLI that takes a filename as an argument and outputs the extracted clueweb
+// information. 
+// takes a second argument for the profiles needed by the language detection
+// module.
 object CluewebExtractorMain extends App {
-  val garbager = new GarbageFilter();
+  // check for the proper number of command line arguments
+  if (args.length != 2) usage
+
+  val garbager = new GarbageFilter(args(1))
   val bp = extractors.ArticleSentencesExtractor.getInstance
   val sentencer = new OpenNlpSentencer()
 
-  // check for the proper number of command line arguments
-  if (args.length != 1) usage();
-
   // get an iterator over the lines of the file specified
-  val warcIt = new WarcEntryIterator(Source.fromFile(args(0), "iso-8859-1").getLines);
+  val warcIt = new WarcEntryIterator(Source.fromFile(args(0), "iso-8859-1").getLines)
 
   while(warcIt.hasNext) {
     val warc = warcIt.next()
-    if (warc != null) {
-      // pass through boilerpipe, sentencer
-      val piped = bp.getText(warc.payload)
-      val sentences = sentencer.sentences(piped)
 
-      // for each sentence, pass through garbager and then print out the:
-      // id, uri, sentence number, sentence
-      // separated by tabs
-      var i = 0;
-      for (sentence <- sentences) {
-        if (!garbager.tooShort(sentence) &&
-            garbager.onlyLatinChars(sentence) &&
-            !garbager.containsHtml(sentence)) {
-          println(warc.warcTrecId + "\t" +
-                  warc.warcTargetUri + "\t" +
-                  i.toString + "\t" +
-                  sentence)
+    if (warc != null) {
+      // pass through boilerpipe
+      val piped = bp.getText(warc.payload)
+
+      // check if the payload is english
+      if (garbager.onlyLatinChars(piped) && garbager.isEnglish(piped)) {
+
+        // split into sentences
+        val sentences = sentencer.sentences(piped)
+
+        // for each sentence, pass through garbager and then print out the:
+        // id, uri, sentence number, sentence
+        // separated by tabs
+        var i = 0;
+        for (s <- sentences) {
+          val sentence = garbager.removeWhitespace(s)
+          if (!garbager.tooShort(sentence) &&
+              !garbager.containsHtml(sentence) &&
+              !garbager.tooLong(sentence)) {
+            println(warc.warcTrecId + "\t" +
+                    warc.warcTargetUri + "\t" +
+                    i.toString + "\t" +
+                    sentence)
+          }
+          i = i + 1;
         }
-        i = i + 1;
       }
     }
   }
 
   def usage() {
-    System.err.println("Usage: java -jar <program> warc-filename");
+    System.err.println("Usage: java -jar <program> warc-filename profiles-directory");
     System.exit(1);
   }
 }
