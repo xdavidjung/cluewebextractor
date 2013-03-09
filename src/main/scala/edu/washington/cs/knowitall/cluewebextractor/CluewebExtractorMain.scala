393080a59461c01d0551e119ae02be79c0c2acd3
David H Jung <davejung@uw>
2013-03-09 15:54:02 -0800

    Messed with lines to make them < 80 chars; messed with the for loop so it iterates over sentences, not a range of numbers.

--- src/main/scala/edu/washington/cs/knowitall/cluewebextractor/CluewebExtractorMain.scala
+++ src/main/scala/edu/washington/cs/knowitall/cluewebextractor/CluewebExtractorMain.scala
@@ -1,13 +1,15 @@
 package edu.washington.cs.knowitall.cluewebextractor
 
+import de.l3s.boilerpipe.extractors
 import edu.washington.cs.knowitall.tool.sentence.OpenNlpSentencer
 import edu.washington.cs.knowitall.common.Timing
 import edu.washington.cs.knowitall.common.Resource
 import edu.washington.cs.knowitall.extractor.HtmlSentenceExtractor
-import edu.washington.cs.knowitall.extractor.mapper.BracketsRemover;
-import edu.washington.cs.knowitall.extractor.mapper.SentenceEndFilter;
-import edu.washington.cs.knowitall.extractor.mapper.SentenceLengthFilter;
-import edu.washington.cs.knowitall.extractor.mapper.SentenceStartFilter;
+import edu.washington.cs.knowitall.extractor.mapper.BracketsRemover
+import edu.washington.cs.knowitall.extractor.mapper.SentenceEndFilter
+import edu.washington.cs.knowitall.extractor.mapper.SentenceLengthFilter
+import edu.washington.cs.knowitall.extractor.mapper.SentenceStartFilter
+import scala.collection.JavaConverters._
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.FileWriter
@@ -65,12 +67,19 @@ object CluewebExtractorMain extends App {
     for (file <- config.inputFiles) {
       val ns = Timing.time {
         Resource.using(openInputStream(file)) { is =>
-          Resource.using(new PrintWriter(config.outputFile(file), "UTF8")) { writer =>
-            val warcIt = new WarcRecordIterator(new DataInputStream(new BufferedInputStream(is)))
+          Resource.using(new PrintWriter(config.outputFile(file), "UTF8")) {
+            writer =>
+              val warcIt = new WarcRecordIterator(
+                             new DataInputStream(
+                             new BufferedInputStream(is))
+                           )
             logger.info("Successfully created new warc iterator")
 
             val garbager = new GarbageFilter()
+
             val nlpSentencer = new OpenNlpSentencer("en-sent.bin")
+            val bp = new extractors.DefaultExtractor()
+
             val hse = new HtmlSentenceExtractor()
             hse.addMapper(new BracketsRemover());
             hse.addMapper(new SentenceEndFilter());
@@ -83,34 +92,45 @@ object CluewebExtractorMain extends App {
               // iterate over warc responses
               warc <- warcIt.flatten
               if warc.warcType.equals("response")
-
-              sentences = hse.extract(warc.payload.trim)
+            } {
+              // val piped = bp.getText(warc.payload.trim)
+              // val sentences = nlpSentencer.segmentTexts(piped)
+              val sentences = hse.extract(warc.payload.trim).asScala
 
               // iterate over sentences
-              i <- 0 until sentences.length
-              sentence = garbager.removeWhitespace(sentences(i))
+              var i = 0
+              for {
+                s <- sentences
 
-              // filter out bad sentences
-              if !garbager.tooShort(sentence);
-              if !garbager.containsHtml(sentence);
-              if !garbager.tooLong(sentence)
-            } {
-              if (warcIt.currentDocument % 100 == 0 && lastDocument != warcIt.currentDocument) {
-                logger.info("Processing: " + warcIt.currentDocument + " (" + ("%.2f" format (warcIt.currentDocument.toDouble / ((System.nanoTime - nanos).toDouble / Timing.Seconds.divisor.toDouble))) + " doc/sec)")
-                lastDocument = warcIt.currentDocument
+                // apply garbage filter
+                sentence = garbager.removeWhitespace(s)
+                if !garbager.tooShort(sentence);
+                if !garbager.containsHtml(sentence);
+                if !garbager.tooLong(sentence)
+              } {
+                if (warcIt.currentDocument % 100 == 0 &&
+                    lastDocument != warcIt.currentDocument) {
+                  logger.info("Processing: " + warcIt.currentDocument + " (" +
+                              ("%.2f" format (warcIt.currentDocument.toDouble /
+                              ((System.nanoTime - nanos).toDouble /
+                              Timing.Seconds.divisor.toDouble))) + " doc/sec)")
+                  lastDocument = warcIt.currentDocument
+                }
+
+                writer.println(warc.warcTrecId + "\t" +
+                               warc.warcUri + "\t" +
+                               warc.warcDate + "\t" +
+                               i + "\t" +
+                               sentence)
+                i += 1
               }
-
-              writer.println(warc.warcTrecId + "\t" +
-                warc.warcUri + "\t" +
-                warc.warcDate + "\t" +
-                i + "\t" +
-                sentence)
             }
           }
         }
       }
 
-      logger.info("Processed file '" + file.getName + "' in: " + Timing.Seconds.format(ns))
+      logger.info("Processed file '" + file.getName + "' in: " +
+                  Timing.Seconds.format(ns))
     }
   }
 
