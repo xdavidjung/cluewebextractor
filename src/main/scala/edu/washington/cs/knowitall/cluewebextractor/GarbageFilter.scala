374caab64f0dce2397807e0075d203d590b76ea7
David H Jung <davejung@uw>
2013-03-09 16:41:31 -0800

    Merge with new garbage filter.

--- src/main/scala/edu/washington/cs/knowitall/cluewebextractor/GarbageFilter.scala
+++ src/main/scala/edu/washington/cs/knowitall/cluewebextractor/GarbageFilter.scala
@@ -1,65 +1,38 @@
 package edu.washington.cs.knowitall.cluewebextractor
 import org.jsoup.Jsoup;
 import scala.util.matching.Regex;
-import com.cybozu.labs.langdetect.Detector;
-import com.cybozu.labs.langdetect.DetectorFactory;
-import com.cybozu.labs.langdetect.LangDetectException;
-import com.cybozu.labs.langdetect.Language;
 
 // this class contains a number of methods that detect garbage in sentences
 // taken from the web.
-class GarbageFilter(dir: String) {
-
-  // initialize the DetectorFactory with the default profile
-  DetectorFactory.loadProfile(dir);
+class GarbageFilter() {
 
   // removes series of extraneous whitespace in the input sentence
   def removeWhitespace(input: String): String = {
     new Regex("""\s+""").replaceAllIn(input, " ")
   }
 
-  // returns whether all the characters in input are within an acceptable
-  // range of characters (common english chars).
-  def onlyLatinChars(input: String): Boolean = {
-    for (char <- input) {
-      // safe chars: 0 to 126, 160 to 190, 8211 to 8230, 8482 
-      if (((char.toInt > 126 && char.toInt < 160) ||
-          (char.toInt > 190 && char.toInt < 8211)) &&
-          (char.toInt != 8482)) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  def isEnglish(input: String): Boolean = {
-    try {
-      val detector = DetectorFactory.create();
-      detector.append(input);
-      detector.detect().equals("en")
-
-    } catch {
-      // if a langdetectexception is thrown, err on the side of leniency:
-      case e: LangDetectException => return true;
-    }
-  }
-
   // returns true if input contains HTML content
   def containsHtml(input: String): Boolean = {
-    input.startsWith("HTTP/1.1") ||  // is an HTTP request
-    input.contains("</") ||  // closing tags
-    input.contains("/>") ||  // self-closing tags
-    input.contains("<a href=") ||  // anchor tags
-    input.matches(".*<.{1,4}>.*")  // opening tags, 1-4 chars
+    input.startsWith("HTTP/1.1") || // is an HTTP request
+      input.contains("</") || // closing tags
+      input.contains("/>") || // self-closing tags
+      input.contains("<a href=") || // anchor tags
+      input.matches(".*<.{1,4}>.*") // opening tags, 1-4 chars
   }
 
-  // returns true if input contains less than three words (detected by the
+  // returns true if input contains less than six words (detected by the
   // number of spaces in input)
   def tooShort(input: String): Boolean = {
-    // split on whitespace and count what's left
-    val split = input.split("\\s+");
-
-    split.length < 3
+    // iterate through and find spaces
+    var numSpaces = 0
+    for {
+      c <- input
+      if c == ' '
+    } {
+      numSpaces += 1
+      if (numSpaces >= 5) return false
+    }
+    true
   }
 
   // returns true if the input is too long: more than 300 chars.
