8edf08fb2afba967b4de80f22f6cbf760a79ad5c
David H Jung <davejung@uw>
2013-04-01 19:23:04 -0700

    Added a lot of try-catch blocks -- bad style (in my mind), but it'll work.

--- src/main/scala/edu/washington/cs/knowitall/cluewebextractor/GarbageFilter.scala
+++ src/main/scala/edu/washington/cs/knowitall/cluewebextractor/GarbageFilter.scala
@@ -6,33 +6,49 @@ import scala.util.matching.Regex;
 // taken from the web.
 class GarbageFilter() {
 
-  // removes series of extraneous whitespace in the input sentence
+  // Removes series of extraneous whitespace in the input sentence
+  // On error simply returns the input
   def removeWhitespace(input: String): String = {
-    new Regex("""\s+""").replaceAllIn(input, " ")
+    try {
+      new Regex("""\s+""").replaceAllIn(input, " ")
+    } catch {
+      case e: Throwable =>
+        input
+    }
   }
 
-  // returns true if input contains HTML content
+  // Returns true if input contains HTML content, false otherwise.
+  // On error, returns true
   def containsHtml(input: String): Boolean = {
-    input.startsWith("HTTP/1.1") || // is an HTTP request
-      input.contains("</") || // closing tags
-      input.contains("/>") || // self-closing tags
-      input.contains("<a href=") || // anchor tags
-      input.matches(".*<.{1,4}>.*") // opening tags, 1-4 chars
+    try {
+      input.startsWith("HTTP/1.1") || // is an HTTP request
+        input.contains("</") || // closing tags
+        input.contains("/>") || // self-closing tags
+        input.contains("<a href=") || // anchor tags
+        input.matches(".*<.{1,4}>.*") // opening tags, 1-4 chars
+    } catch {
+      case e: Throwable =>
+        true
+    }
   }
 
-  // returns true if input contains less than six words (detected by the
+  // Returns true if input contains less than six words (detected by the
   // number of spaces in input)
   def tooShort(input: String): Boolean = {
-    // iterate through and find spaces
-    var numSpaces = 0
-    for {
-      c <- input
-      if c == ' '
-    } {
-      numSpaces += 1
-      if (numSpaces >= 5) return false
+    try {
+      // iterate through and find spaces
+      var numSpaces = 0
+      for (c <- input; if c == ' ') {
+        numSpaces += 1
+        if (numSpaces >= 5) {
+          return false
+        }
+      }
+      true
+    } catch {
+      case e: Throwable =>
+        true
     }
-    true
   }
 
   // returns true if the input is too long: more than 300 chars.
