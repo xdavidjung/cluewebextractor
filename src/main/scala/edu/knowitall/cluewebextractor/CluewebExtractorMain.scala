e457085aa5810fb7803fc6daed39477894416f5f
David H Jung <xdavidjung@gmail.com>
2013-04-09 11:46:30 -0700

    Moving to new package.

---
+++ src/main/scala/edu/knowitall/cluewebextractor/CluewebExtractorMain.scala
@@ -0,0 +1,225 @@
+package edu.knowitall.cluewebextractor
+
+import de.l3s.boilerpipe.extractors
+import edu.washington.cs.knowitall.tool.sentence.OpenNlpSentencer
+import edu.washington.cs.knowitall.common.Timing
+import edu.washington.cs.knowitall.common.Resource
+import scala.collection.JavaConverters._
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.FileWriter
+import java.io.PrintStream
+import java.io.InputStream
+import java.io.FileInputStream
+import java.io.File
+import java.io.PrintWriter
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.DataInputStream
+import java.util.zip.GZIPInputStream
+
+/**
+ *    Copyright 2013 David H Jung
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ----------------------- END OF LICENSE INFO --------------------------------
+ *
+ * CLI that takes as input either a .warc file or directory containing .warc
+ * files and outputs the extracted payload content in either a default or
+ * specified directory.
+ *
+ * If the user inputs a directory that contains a .warc file with an already-
+ * existing corresponding output file, it will be skipped.
+ *
+ * If the user inputs a single .warc file with an already-existing
+ * corresponding output file, it will be overwritten.
+ *
+ * On bad input or unexpected errors, this program will choose to log the error
+ * and skip over the file (or document, depending on the granularity of the
+ * error) rather than stop execution.
+ */
+object CluewebExtractorMain extends App {
+  val logger = LoggerFactory.getLogger(this.getClass)
+
+  case class Config(
+    inputFiles: Seq[File] = Seq.empty,
+    outputDirectory: Option[File] = None) {
+  }
+
+  // Defines the command line arguments.
+  val parser = new scopt.immutable.OptionParser[Config]("cweb") {
+    def options = Seq(
+      arglist("<input-files>", "pattern file") { (path: String, config: Config) =>
+        val file = new File(path)
+        require(file.exists(), "file does not exist: " + path)
+        config.copy(inputFiles = (config.inputFiles :+ file))
+      },
+      opt("output-dir", "output directory") { (path: String, config: Config) =>
+        val file = new File(path)
+        require(file.exists, "directory does not exist: " + path)
+        require(file.isDirectory, "file is not a directory: " + path)
+        config.copy(outputDirectory = Some(file))
+      })
+  }
+
+  parser.parse(args, Config()) match {
+    case Some(config) => run(config)
+    case None => System.err.println(usage)
+  }
+
+  def run(config: Config) {
+
+    // Output filename is the input filename up to and including the first dot
+    // with "sentences" as the extension.
+    def makeOutputFileName(inputFile: File) = {
+      inputFile.getName().takeWhile(_ != '.') + ".sentences"
+    }
+
+    // Files contains (inputFile, outputFile) pairs.
+    val files: Iterable[(File, File)] = config.inputFiles.flatMap { file =>
+      import org.apache.commons.io.FileUtils
+      import scala.collection.JavaConverters._
+
+      // if it's a directory, search subdirectories
+      if (file.isDirectory) {
+        val files: Iterable[File] =
+          FileUtils.listFiles(file, Array("gz"), true).asScala
+
+        files.flatMap { inputFile =>
+          val subdirectory = inputFile.getParentFile.getPath.drop(file.getParentFile.getPath.length).drop(1)
+
+          // build the output file
+          val outputDirectory = config.outputDirectory match {
+            case Some(dir) => new File(dir, subdirectory)
+            case None => new File(subdirectory)
+          }
+
+          // create the file's parent directory if it doesn't exist
+          outputDirectory.mkdirs
+
+          val outputFileName = makeOutputFileName(inputFile)
+          val outputFile = new File(outputDirectory, outputFileName)
+
+          // if the output file already exists, skip by returning None
+          if (outputFile.exists) {
+            None
+          } else {
+            Some(inputFile, outputFile)
+          }
+        }
+
+      } else {
+        // the user input a simple .warc file
+        val outputFileName = makeOutputFileName(file)
+        val outputFile = config.outputDirectory match {
+          case Some(dir) => new File(dir, outputFileName)
+          case None => new File(outputFileName)
+        }
+        Some(file, outputFile)
+      }
+    }
+
+    // Create the warc record processors
+    val garbager = new GarbageFilter()
+    val nlpSentencer = new OpenNlpSentencer()
+    val bp = new extractors.DefaultExtractor()
+
+    // For each (input, output) pair, get a warc record iterator for the input
+    // and write the corresponding extracted payload to the output
+    for ((inputFile, outputFile) <- files) {
+      val ns = Timing.time {
+      Resource.using(openInputStream(inputFile)) { is =>
+      Resource.using(new PrintWriter(outputFile, "UTF8")) { writer =>
+
+        val warcIt = new WarcRecordIterator(
+                       new DataInputStream(
+                       new BufferedInputStream(is))
+                     )
+        logger.info("Successfully created new warc iterator")
+
+        var lastDocument = 0
+        var nanos = System.nanoTime()
+
+        // Iterate over warc documents
+        for (warc <- warcIt.flatten;
+             if warc.warcType.equals("response") &&
+                !warc.payload.equals("")) {
+          // If this document is a multiple of a thousand, note it in the log
+          // and the current documents / second
+          if (warcIt.currentDocument % 1000 == 0 &&
+              lastDocument != warcIt.currentDocument) {
+            logger.info("Processing document: " + warcIt.currentDocument +
+                        " (" +
+                        ("%.2f" format (warcIt.currentDocument.toDouble /
+                        ((System.nanoTime - nanos).toDouble /
+                        Timing.Seconds.divisor.toDouble))) + " doc/sec)")
+            lastDocument = warcIt.currentDocument
+          }
+
+          // piped stores the payload after being passed through boilerpipe
+          val piped = try {
+            bp.getText(warc.payload.trim)
+          } catch {
+            case e: Throwable =>
+              logger.error("Error during boilerpipe extraction.\n" +
+                           "Skipping document\n" + e.getStackTraceString)
+              ""
+          }
+
+          val sentences = nlpSentencer.segmentTexts(piped)
+
+          // iterate over sentences
+          var i = 0
+          for {
+            s <- sentences
+
+            // apply garbage filter
+            sentence = garbager.removeWhitespace(s)
+            if !garbager.containsHtml(sentence);
+            if !garbager.tooLong(sentence);
+            if !garbager.tooShort(sentence)
+          } {
+            writer.println(warc.warcTrecId + "\t" +
+                           warc.warcUri + "\t" +
+                           warc.warcDate + "\t" +
+                           i + "\t" +
+                           sentence)
+            i += 1
+
+          }
+        }
+      }}}
+
+      logger.info("Processed file '" + inputFile.getName + "' -> '"
+          + outputFile.getName + "' in: " + Timing.Seconds.format(ns))
+    }
+  }
+
+  def usage {
+    "Usage: java -jar <this.jar> <input.warc(.gz)>"
+  }
+
+  def openInputStream(file: File): InputStream = {
+    if (file.getName endsWith ".gz") {
+      // then the user has passed in .warc.gz file
+      logger.info("Opening zip file " + file)
+      new GZIPInputStream(new FileInputStream(file))
+    } else {
+      // then the user has passed in .warc file
+      logger.info("Opening file " + file)
+      new FileInputStream(file)
+    }
+  }
+}
---
+++ src/main/scala/edu/knowitall/cluewebextractor/WarcRecordIterator.scala
@@ -0,0 +1,214 @@
+package edu.knowitall.cluewebextractor
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import java.io.DataInputStream
+
+/**
+ * This class provides a way to iterate over the WARC records in a ClueWeb12
+ * .warc file. This means that it is assumed that the format of the WARC
+ * records will match up with those described at:
+ *
+ *   http://bibnum.bnf.fr/WARC/WARC_ISO_28500_version1_latestdraft.pdf
+ *
+ * It is possible for an error to occur when reading from the .warc file. In
+ * this relatively rare case, the iterator will become invalid; if this occurs
+ * in a call to next(), the call will return a WarcRecord with no payload.
+ */
+class WarcRecordIterator(dis: DataInputStream) extends Iterator[Option[WarcRecord]] {
+  var byteBuffer = Array[Byte](0)
+
+  // The number of documents in this warc file
+  private var _numberOfDocuments: Int = -1
+  def numberOfDocuments = _numberOfDocuments
+  private def numberOfDocuments_=(x0: Int) { _numberOfDocuments = x0 }
+
+  // The current document. Used for debugging.
+  private var _currentDocument: Int = -1
+  def currentDocument = _currentDocument
+  private def currentDocument_=(x0: Int) { _currentDocument = x0 }
+
+  // The latest line that has been read from fileBytes.
+  private var current: String = null
+
+  // Whether this iterator is valid.
+  private var valid = true
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  initialize()
+
+  // Returns true if there is another warc entry in file.
+  // Can mutate the state of fileBytes: if hasNext returns true,
+  // then fileBytes will be at the beginning of the next warcEntry.
+  def hasNext(): Boolean = {
+    if (!valid) {
+      valid
+    }
+
+    // a warc file has a next warcEntry if we can find a line in fileBytes that
+    // is equal to recordStarter
+    while (valid && !current.equals(WarcRecordIterator.recordStarter)) {
+      nextLine()
+      if (current == null) {
+        logger.info("Finished reading warc file at document " + currentDocument +
+          "/" + numberOfDocuments)
+        if (currentDocument != numberOfDocuments) {
+          logger.error("No more entries but currentDocument is not " +
+            "numberOfDocuments: " + currentDocument + "/" +
+            numberOfDocuments)
+        }
+        valid = false
+      }
+    }
+    valid
+  }
+
+  // Returns the next WarcRecord in this file.
+  // throws NoSuchElementException if there is no next WarcRecord.
+  def next(): Option[WarcRecord] = {
+    if (!hasNext) {
+      throw new NoSuchElementException()
+    }
+
+    logger.debug("Getting next WARC record. Current document: " +
+      currentDocument)
+
+    // Grab lines and process fields until we hit the end of block indicator
+    var warcFieldMap = Map.empty[String, String]
+    nextLine()
+    while (!current.isEmpty) {
+      val splitLine = current.split(": ")
+
+      // all lines until the end of the block should be splittable
+      if (splitLine.length != 2) {
+        logger.error("Bad WARC header: no ': ' sequence in document: " +
+          currentDocument + " on line: " + current)
+        currentDocument += 1
+        return None
+      }
+
+      warcFieldMap += splitLine(0) -> splitLine(1)
+      nextLine()
+    }
+
+    // Get the fields we want
+    val warcType = warcFieldMap(WarcRecordIterator.typeIndicator)
+    val warcTrecId = warcFieldMap(WarcRecordIterator.trecIdIndicator)
+    val warcDate = warcFieldMap(WarcRecordIterator.dateIndicator)
+    val warcUri = warcFieldMap(WarcRecordIterator.uriIndicator)
+
+    var contentLength = try {
+      warcFieldMap(WarcRecordIterator.contentLengthIndicator).toInt
+    } catch {
+      case e: NumberFormatException =>
+        logger.error("Unable to convert content length to int with string: " +
+          warcFieldMap(WarcRecordIterator.contentLengthIndicator) +
+          " in document: " + currentDocument)
+        currentDocument += 1
+        return None
+    }
+
+    // Get the payload
+    byteBuffer = new Array[Byte](contentLength)
+    try {
+      dis.read(byteBuffer, 0, contentLength)
+    } catch {
+      case e: Throwable =>
+        logger.error("Exception caught while reading payload from .warc " +
+                     "file.\nCurrent document: " + currentDocument + "\n" +
+                     e.getStackTraceString)
+        valid = false;
+        byteBuffer = new Array[Byte](0)
+        contentLength = 0
+    }
+
+    currentDocument += 1
+    new Some(WarcRecord(warcType, warcTrecId, warcDate, warcUri,
+                        new String(byteBuffer, 0, contentLength, "UTF8")))
+
+  }
+
+  // Gets the next line of input and stores it in current.
+  // Returns null on EOF or error.
+  private def nextLine(): String = {
+    current = try {
+      dis.readLine()
+    } catch {
+      case e: Throwable =>
+        logger.error("Error while reading next line from .warc file.\n" +
+                     "Current document: " + currentDocument + "\n" +
+                     e.getStackTraceString)
+        valid = false;
+        ""
+    }
+    current
+  }
+
+  // Opens the file given in the constructor and slurps up the warcinfo header
+  // for this file, setting fileBytes to the beginning of the first warc record.
+  private def initialize(): Unit = {
+    // just a quick check that this is indeed a warc file:
+    if (!(nextLine() equals WarcRecordIterator.recordStarter)) {
+      logger.error("Input first line is not " +
+        WarcRecordIterator.recordStarter + " but rather " + current)
+      valid = false
+    }
+    if (!(nextLine() equals WarcRecordIterator.headerType)) {
+      logger.error("Input second line is not " +
+        WarcRecordIterator.headerType + " but rather " + current)
+      valid = false
+    }
+
+    // now process the header: the goal is to get the number of docs and set
+    // the file iterator at the beginning of the first actual warc record
+    currentDocument = 0
+    nextLine()
+    while (!current.equals(WarcRecordIterator.recordStarter)) {
+      if (current startsWith WarcRecordIterator.numDocField) {
+        try {
+          numberOfDocuments = current.split(": ")(1).toInt
+        } catch {
+          case e: NumberFormatException =>
+            logger.error("Unable to convert document total to int with " +
+              "string: " + current.split(": ")(1))
+        }
+      }
+      nextLine()
+    }
+    if (numberOfDocuments == -1) {
+      logger.error("Number of documents not found for warc file")
+    }
+  }
+}
+
+/**
+ * Companion object used to store the various content indicators in .warc files
+ */
+object WarcRecordIterator {
+  // When found alone, indicates the start of a new WARC record.
+  val recordStarter = "WARC/1.0"
+
+  // The type of the header WARC record, present at the beginning of
+  // all .warc files.
+  val headerType = "WARC-Type: warcinfo"
+
+  // Field in the warcinfo header for the number of documents in this warc file
+  val numDocField = "WARC-Number-of-Documents"
+
+  // indicates the field that gives an id
+  val trecIdIndicator = "WARC-TREC-ID"
+
+  // indicates the field that gives a date
+  val dateIndicator = "WARC-Date"
+
+  // indicates the field that gives a uri
+  val uriIndicator = "WARC-Target-URI"
+
+  // indicates the field that gives the warc type of the record
+  val typeIndicator = "WARC-Type"
+
+  // indicates the field that gives content length
+  val contentLengthIndicator = "Content-Length"
+}
